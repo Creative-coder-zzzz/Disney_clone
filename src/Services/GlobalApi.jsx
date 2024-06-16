4399c8a2911f8a1dc679963329da7ac5d13f0632
Sandesh Adhikari <sandeshadhikari2003@gmail.com>
2024-06-16 20:56:38 +0530

    updated services

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,17 +1,18 @@
 import axios from "axios";
 
-const movieBaseUrl="https://api.themoviedb.org/3"
-const api_key='2ec0d66f5bdf1dd12eefa0723f1479cf'
+const movieBaseUrl = "https://api.themoviedb.org/3";
+const api_key = "3b2939de8803a8843c7f2e819363a486";
 
-const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf';
+const movieByGenreBaseURL =
+  "https://api.themoviedb.org/3/discover/movie?api_key=3b2939de8803a8843c7f2e819363a486";
 
-//https://api.themoviedb.org/3/trending/all/day?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf
-const getTrendingVideos=axios.get(movieBaseUrl+
-    "/trending/all/day?api_key="+api_key);
-    const getMovieByGenreId=(id)=>
-    axios.get(movieByGenreBaseURL+"&with_genres="+id)
+const getTrendingVideos = axios.get(
+  movieBaseUrl + "/trending/all/day?api_key=" + api_key
+);
+const getMovieByGenreId = (id) =>
+  axios.get(movieByGenreBaseURL + "&with_genres=" + id);
 
-export default{
-    getTrendingVideos,
-    getMovieByGenreId
-}
+export default {
+  getTrendingVideos,
+  getMovieByGenreId,
+};
